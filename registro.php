885c0305fc82802d700aee896ba3b91bb4f5e2d7
José Pazmiño <pepe.paz0399@gmail.com>
2021-06-10 02:30:09 -0500

    Implementación Base de datos
    -ingreso de datos de los usuarios funcional
    -se movió el archivo bd_for_grup.sql a la raíz del proyecto debido a la implementación.
    -cambios menores en todos los .html (aparición de leyendas al colocar el mouse).

--- registro.php
+++ registro.php
@@ -1,34 +1,50 @@
 <?php
 	//conexion con la base de datos y el servidor
-	$link = mysql_connect("localhost","root","") or die("<h2>No se encuentra el servidor</h2>");
-	$db = mysql_select_db("formulario",$link) or die("<h2>Error de Conexion</h2>");
+	$connect = mysqli_connect("localhost","root","");
 
-	//obtenemos los valores del formulario
-	$nombres = $_POST['nombreuser'];
-	$apellidos = $_POST['apellidosuser'];
-	$email = $_POST['emailuser'];
-	$pass = $_POST['pass'];
-	$rpass = $_POST['rpass'];
+	if(!$connect) 
+        {
+            echo "No se ha podido conectar con el servidor" ;
+        }
+  else 
+ 		{
 
-	//Obtiene la longitus de un string
-	$req = (strlen($nombres)*strlen($apellidos)*strlen($email)*strlen($pass)*strlen($rpass)) or die("No se han llenado todos los campos");
+	$db = "bd_for_grup";
+	$sdb = mysqli_select_db($connect,$db); 
+		}
 
-	//se confirma la contraseña
-	if ($pass != $rpass) {
-		die('Las contraseñas no coinciden, Verifique <br /> <a href="index.html">Volver</a>');
-	}
+if(!$sdb)
+		{
+	echo "No se encontro la base de datos" ;
+		}				
+else
+		{
+//obtenemos los valores del formulario
+$name = $_POST['username'];
+$surname = $_POST['usersurname'];
+$phone= $_POST['phone'];
+$user= $_POST['user'];
+$email = $_POST['useremail'];
+$pass = $_POST['userpassword'];
 
-	//se encripta la contraseña
-	$contraseñaUser = md5($pass);
+//$rpass = $_POST['rpass'];
 
-	//ingresamos la informacion a la base de datos
-	mysql_query("INSERT INTO datos VALUES('','$nombres','$apellidos','$email','$contraseñaUser')",$link) or die("<h2>Error Guardando los datos</h2>");
-	echo'
-		<script>
-			alert("Registro Exitoso");
-			location.href="index.html";
-		</script>
-	'
+//Obtiene la longitus de un string
 
+//se confirma la contraseña
+/*if ($pass != $rpass) {
+	die('Las contraseñas no coinciden, Verifique <br /> <a href="index.html">Volver</a>');
+}*/
 
+//se encripta la contraseña
+//$contraseñaUser = md5($pass);
+//ingresamos la informacion a la base de datos
+$insert="INSERT INTO usuario
+(COD_USU, NOM_USU, APE_USU,COR_USU,TEL_USU,NIC_USU,CON_USU,ADM_USU) VALUES('','$name','$surname','$email','$phone','$user','$pass',NULL)";
+
+mysqli_query($connect,$insert);
+
+mysqli_close($connect);
+		}
+echo'<script>alert("Registro Exitoso");location.href="index.html";</script>';
 ?>
